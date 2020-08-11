f23db325c7948940c701f338e741d579785a7403
Segolene de la Taille <segolette@hotmail.com>
2020-08-11 15:01:43 +0200

    exo 2 ok

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+  before_action :get_task_by_id, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def get_task_by_id
+    @task = Task.find(params[:id])
+  end
+
+end
